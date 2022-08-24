44af1bce0d245626c4d511fcdf0d8e7bff348229
Alexander Kukushkin <alex@alexkuk.ru>
2022-08-24 17:27:59 +0300

    Init add agree participate

--- neludim/ops.py
+++ neludim/ops.py
@@ -1,17 +1,24 @@
 
 from .const import (
+    PARTICIPATE_COMMAND,
+    PAUSE_WEEK_COMMAND,
+    PAUSE_MONTH_COMMAND,
     CONFIRM_CONTACT_COMMAND,
     FAIL_CONTACT_COMMAND,
     CONTACT_FEEDBACK_COMMAND,
 
     CONFIRM_STATE,
-    FAIL_STATE
+    FAIL_STATE,
+
+    WEEK,
+    MONTH,
 )
 from .text import (
+    day_month,
     user_url,
     user_mention
 )
-
+from .schedule import week_index
 from .bot.broadcast import (
     BroadcastTask,
     broadcast
@@ -25,14 +32,24 @@ from .bot.broadcast import (
 ####
 
 
-def confirm_contact_text(user):
+def ask_agree_participate_text(schedule):
+    return f'''Участвуешь во встречах на следующей неделе? Бот будет подбирать пары в понедельник {day_month(schedule.next_week_monday())}.
+
+/{PARTICIPATE_COMMAND} - участвую
+/{PAUSE_WEEK_COMMAND} - пауза на неделю
+/{PAUSE_MONTH_COMMAND} - пауза не месяц
+
+Бот просит подтверждать участие каждую неделю. Подбирает пару только из тех, кто согласился. Это уменьшает число несостоявшихся встреч.'''
+
+
+def ask_confirm_contact_text(user):
     return f'''Получилось договориться с <a href="{user_url(user.user_id)}">{user_mention(user)}</a> о встрече?
 
 /{CONFIRM_CONTACT_COMMAND} - да, договорились
 /{FAIL_CONTACT_COMMAND} - нет, не договорились/не отвечает'''
 
 
-def contact_feedback_text(user):
+def ask_contact_feedback_text(user):
     return f'''Оставь, пожалуйста, фидбек о встрече с <a href="{user_url(user.user_id)}">{user_mention(user)}</a>.
 
 /{CONTACT_FEEDBACK_COMMAND} - оставить фидбек
@@ -63,12 +80,45 @@ def find_user(users, user_id=None, username=None):
             return user
 
 
-async def confirm_contact_op(context):
+async def ask_agree_participate(context):
+    users = await context.db.read_users()
+    next_week_index = context.schedule.current_week_index() + 1
+
+    tasks = []
+    for user in users:
+        if user.paused:
+            if user.pause_period == WEEK:
+                offset = 1
+            elif user.pause_period == MONTH:
+                offset = 4
+
+            if next_week_index <= week_index(user.paused) + offset:
+                continue
+
+        if (
+                user.agreed_participate
+                and week_index(user.agreed_participate) + 1 == next_week_index
+        ):
+            # Alread agreed to participate next week
+            continue
+
+        text = ask_agree_participate_text(context.schedule)
+        tasks.append(BroadcastTask(
+            chat_id=user.user_id,
+            text=text
+        ))
+
+    await broadcast(context.bot, tasks)
+
+
+async def ask_confirm_contact(context):
     users = await context.db.read_users()
 
     contacts = await context.db.read_contacts()
-    week_id = context.schedule.current_week_index()
-    contacts = find_contacts(contacts, week_index=week_id)
+    contacts = find_contacts(
+        contacts,
+        week_index=context.schedule.current_week_index()
+    )
 
     tasks = []
     skip_user_ids = set()
@@ -86,7 +136,7 @@ async def confirm_contact_op(context):
             continue
 
         partner_user = find_user(users, user_id=contact.partner_user_id)
-        text = confirm_contact_text(partner_user)
+        text = ask_confirm_contact_text(partner_user)
         tasks.append(BroadcastTask(
             chat_id=contact.user_id,
             text=text
@@ -95,12 +145,14 @@ async def confirm_contact_op(context):
     await broadcast(context.bot, tasks)
 
 
-async def contact_feedback_op(context):
+async def ask_contact_feedback(context):
     users = await context.db.read_users()
 
     contacts = await context.db.read_contacts()
-    week_id = context.schedule.current_week_index()
-    contacts = find_contacts(contacts, week_index=week_id)
+    contacts = find_contacts(
+        contacts,
+        week_index=context.schedule.current_week_index()
+    )
 
     tasks = []
     for contact in contacts:
@@ -111,7 +163,7 @@ async def contact_feedback_op(context):
             continue
 
         partner_user = find_user(users, user_id=contact.partner_user_id)
-        text = contact_feedback_text(partner_user)
+        text = ask_contact_feedback_text(partner_user)
         tasks.append(BroadcastTask(
             chat_id=contact.user_id,
             text=text
--- neludim/tests/test_ops.py
+++ neludim/tests/test_ops.py
@@ -7,27 +7,50 @@ from neludim.tests.fake import (
 from neludim.const import (
     CONFIRM_STATE,
     FAIL_STATE,
+
+    WEEK,
+    MONTH,
 )
 from neludim.obj import (
     User,
     Contact,
 )
+from neludim.schedule import week_index_monday
+    
 from neludim.ops import (
-    confirm_contact_op,
-    contact_feedback_op,
+    ask_agree_participate,
+    ask_confirm_contact,
+    ask_contact_feedback,
 )
 
 
-USERS = [
-    User(user_id=1, username='a'),
-    User(user_id=2, username='b'),
-    User(user_id=3, username='c'),
-    User(user_id=4, username='d'),
-]
+async def test_ask_agree_participate(context):
+    week_index = context.schedule.current_week_index()
+    context.db.users = [
+        User(user_id=0),
+        User(user_id=1, agreed_participate=week_index_monday(week_index - 1)),
+        User(user_id=2, agreed_participate=week_index_monday(week_index)),
+        User(user_id=3, paused=week_index_monday(week_index - 1), pause_period=WEEK),
+        User(user_id=4, paused=week_index_monday(week_index), pause_period=WEEK),
+        User(user_id=5, paused=week_index_monday(week_index - 3), pause_period=MONTH),
+        User(user_id=6, paused=week_index_monday(week_index - 4), pause_period=MONTH),
+    ]
+    await ask_agree_participate(context)
+    assert match_trace(context.bot.trace, [
+        ['sendMessage', '"chat_id": 0'],
+        ['sendMessage', '"chat_id": 1'],
+        ['sendMessage', '"chat_id": 3'],
+        ['sendMessage', '"chat_id": 6'],
+    ])
 
 
-async def test_broadcast_confirm_contact(context):
-    context.db.users = USERS
+async def test_ask_confirm_contact(context):
+    context.db.users = [
+        User(user_id=1, username='a'),
+        User(user_id=2, username='b'),
+        User(user_id=3),
+        User(user_id=4),
+    ]
     context.db.contacts = [
         Contact(week_index=0, user_id=1, partner_user_id=2),
         Contact(week_index=0, user_id=2, partner_user_id=1),
@@ -35,22 +58,27 @@ async def test_broadcast_confirm_contact(context):
         Contact(week_index=0, user_id=4, partner_user_id=3),
     ]
     
-    await confirm_contact_op(context)
+    await ask_confirm_contact(context)
     assert match_trace(context.bot.trace, [
         ['sendMessage', '@b'],
         ['sendMessage', '@a'],
     ])
 
 
-async def test_broadcast_contact_feedback(context):
-    context.db.users = USERS
+async def test_ask_contact_feedback(context):
+    context.db.users = [
+        User(user_id=1),
+        User(user_id=2),
+        User(user_id=3),
+        User(user_id=4, username='d'),
+    ]
     context.db.contacts = [
         Contact(week_index=0, user_id=1, partner_user_id=2, state=FAIL_STATE),
         Contact(week_index=0, user_id=2, partner_user_id=1, feedback='1'),
         Contact(week_index=0, user_id=3, partner_user_id=4, state=CONFIRM_STATE),
     ]
     
-    await contact_feedback_op(context)
+    await ask_contact_feedback(context)
     assert match_trace(context.bot.trace, [
         ['sendMessage', '@d'],
     ])
