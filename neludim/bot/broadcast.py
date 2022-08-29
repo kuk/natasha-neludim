b206d56d52c481c7ab37cfcb60cc992bbc5af95f
Alexander Kukushkin <alex@alexkuk.ru>
2022-08-29 16:51:46 +0300

    Log broadcast send

--- neludim/bot/broadcast.py
+++ neludim/bot/broadcast.py
@@ -4,6 +4,11 @@ from dataclasses import dataclass
 
 from aiogram import exceptions
 
+from neludim.log import (
+    log,
+    json_msg
+)
+
 
 # https://github.com/aiogram/aiogram/blob/dev-2.x/examples/broadcast_example.py
 
@@ -14,8 +19,10 @@ async def send_message(bot, chat_id, text):
     except exceptions.RetryAfter as error:
         await asyncio.sleep(error.timeout)
         await send_message(bot, chat_id, text)
-    except exceptions.TelegramAPIError:
-        pass
+    except exceptions.TelegramAPIError as error:
+        return error.__class__.__name__
+    else:
+        return
 
 
 @dataclass
@@ -35,7 +42,11 @@ class Broadcast:
 
     async def send(self):
         for message in self.queue:
-            await send_message(self.bot, message.chat_id, message.text)
+            error = await send_message(self.bot, message.chat_id, message.text)
+            log.info(json_msg(
+                chat_id=message.chat_id,
+                error=error
+            ))
 
             # safe 20 rps < limit 30 rps
             await asyncio.sleep(0.05)
