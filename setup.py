24c2fb8b35c5ea2a2b51f77b47bf140e51b45707
Alexander Kukushkin <alex@alexkuk.ru>
2023-02-02 11:13:49 +0300

    Ignore notes dir (for py 3.10?)

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='neludim',
+    packages=find_packages(
+        exclude=['notes']
+    ),
     entry_points={
         'console_scripts': [
             'neludim=neludim.cli:main'
